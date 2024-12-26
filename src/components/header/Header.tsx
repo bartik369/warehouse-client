ae96db1c7a8d8661dde236b9575a3610766db2c3
bartik369 <6apmuk@gmail.com>
2024-12-26 17:41:38 +0300

    Sticky header works(before refactoring)

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,18 +1,21 @@
 import { FC} from 'react';
 import HeaderMenu from '../navigates/header/HeaderMenu';
-import style from './Header.module.scss';
 import Search from '../search/Search';
 import BurgerBtn from '../ui/buttons/burger/BurgerBtn';
 import Profile from '../profile/Profile';
+import style from './Header.module.scss';
 
 interface IHeaderProps {
   isActive: boolean;
   setIsActive: (isActive:boolean) => void;
+  isSticky: boolean;
 };
 
-const Header:FC<IHeaderProps> = ({isActive, setIsActive}) => {
+const Header:FC<IHeaderProps> = ({isActive, setIsActive, isSticky}) => {
     return (
-      <header className={style.header}>
+      <header  className={`${style.header} ${isSticky 
+        ? style.sticky 
+        : style.relative}`} >
         <BurgerBtn isActive={isActive} action={() => setIsActive(!isActive)}/>
         <HeaderMenu />
         <Search />
--- src/routes/Layout.tsx
+++ src/routes/Layout.tsx
@@ -1,4 +1,4 @@
-import React,{ useEffect, useState, useRef} from 'react';
+import { useEffect, useState} from 'react';
 import { Outlet } from 'react-router-dom';
 import Footer from '../components/footer/Footer';
 import Header from '../components/header/Header';
@@ -7,15 +7,30 @@ import style from './Layout.module.scss';
 
 const Layout = () => {
     const [isActive, setIsActive] = useState(false);
+    const [isSticky, setIsSticky] = useState<boolean>(false);
+  
+    useEffect(() => {
+        const handleScroll = () => {
+          if (window.scrollY > 60) {
+            setIsSticky(true);
+          } else {
+            setIsSticky(false);
+          }
+        };
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+          window.removeEventListener('scroll', handleScroll);
+        };
+      }, []);
+
+    console.log('rerender');
     
+
     return (
         <div className={style.wrapper}>
             <Sidebar open={isActive} />
-            <div className={style.content}>
-                <Header 
-                isActive={isActive} 
-                setIsActive={setIsActive} 
-                />
+            <div className={isActive ? style.active : style['not-active']}>
+                <Header isActive={isActive} setIsActive={setIsActive} isSticky={isSticky}/>
                 <div className={style.outlet}>
                     <Outlet /> 
                 </div>
