d4ea1c264c3bbb2609ceea9292a7a04cfa436347
bartik369 <6apmuk@gmail.com>
2025-02-09 22:04:21 +0300

    testing new version of devices filter

--- src/hooks/data/useDeviceFilters.ts
+++ src/hooks/data/useDeviceFilters.ts
@@ -1,8 +1,8 @@
-import { CheckedDeviceOptions } from '../../types/content';
-import { IFilteredDevicesFromBack, IDeviceFilters, IFilterDeviceOptions } from './../../types/devices';
 import { useState, useEffect, ChangeEvent } from "react";
 import { useSearchParams } from "react-router-dom";
 import { useGetDevicesQuery, useGetDeviceOptionsQuery } from "../../store/api/devicesApi";
+import { CheckedDeviceOptions } from "../../types/content";
+import { IDeviceFilters, IFilterDeviceOptions, IFilteredDevicesFromBack } from './../../types/devices';
 
 export const useDeviceFilters = () => {
   const [searchParams, setSearchParams] = useSearchParams();
@@ -16,15 +16,14 @@ export const useDeviceFilters = () => {
     model: [],
     warehouse: [],
   });
-
   const [disabledOptions, setDisabledOptions] = useState<Record<string, string[]>>({});
   const { data: devices } = useGetDevicesQuery(Object.fromEntries(searchParams));
   const { data: options } = useGetDeviceOptionsQuery();
 
   useEffect(() => {
-    if (!devices || !options) return;
-
-    const newDisabledOptions: Record<string, string[]> = {
+    if (!options) return;
+    
+    const disabledOptions: Record<string, string[]> = {
       manufacturer: [],
       type: [],
       model: [],
@@ -34,52 +33,52 @@ export const useDeviceFilters = () => {
       isFunctional: [],
       isAssigned: [],
     };
-
-    const availableValues: Record<string, string[]> = {
-      manufacturer: Array.from(
-        new Set(devices?.map((device: IFilteredDevicesFromBack) => device.model.manufacturer?.slug).filter(Boolean))
-      ),
-      type: Array.from(
-        new Set(devices?.map((device: IFilteredDevicesFromBack) => device.model.type?.slug).filter(Boolean))
-      ),
-      model: Array.from(
-        new Set(devices?.map((device: IFilteredDevicesFromBack) => device.model?.slug).filter(Boolean))
-      ),
-      warehouse: Array.from(
-        new Set(devices?.map((device: IFilteredDevicesFromBack) => device.warehouse?.slug).filter(Boolean))
-      ),
-      memorySize: Array.from(
-        new Set(devices?.map((device: IFilteredDevicesFromBack) => device?.memorySize?.toString()))
-      ),
-      screenSize: Array.from(
-        new Set(devices?.map((device: IFilteredDevicesFromBack) => device?.screenSize?.toString()))
-      ),
-      isFunctional: Array.from(
-        new Set(devices?.map((device: IFilteredDevicesFromBack) => String(device.isFunctional)))
-      ),
-      isAssigned: Array.from(
-        new Set(devices?.map((device: IFilteredDevicesFromBack) => String(device.isAssigned)))
-      ),
+    // Get all device options
+    const devicesOptions: Record<string, string[]> = {
+      manufacturer: options.manufacturer?.map((item) => item.slug) || [],
+      type: options.type?.map((item) => item.slug) || [],
+      model: options.model?.map((item) => item.slug) || [],
+      memorySize: options.memorySize?.map((item) => String(item.memorySize)) || [],
+      screenSize: options.screenSize?.map((item) => String(item.screenSize)) || [],
+      warehouse: options.warehouse?.map((item) => item.slug) || [],
+      isFunctional: ["true", "false"],
+      isAssigned: ["true", "false"],
     };
-   
-    Object.entries(availableValues).forEach(([key, values]) => {
-      const optionList = options[key as keyof IFilterDeviceOptions] as Array<any> || [];
-    
-      newDisabledOptions[key] = optionList
-        .map((item) => [
-          item.slug?.toString(),
-          item.memorySize !== undefined ? String(item.memorySize) : undefined,
-          item.screenSize !== undefined ? String(item.screenSize) : undefined,
-          item.isAssigned === true ? "true" : item.isAssigned === false ? "false" : undefined,
-          item.isFunctional === true ? "true" : item.isFunctional === false ? "false" : undefined
-        ])
-        .flat()
-        .filter((value): value is string => value !== undefined && !values.includes(value));
-    });
-    
-    setDisabledOptions(newDisabledOptions);
-  }, [devices, options]);
+    //Checking available options by selected filters
+    Object.entries(devicesOptions).forEach(([key, optionList]) => {
+      //Filtering options that cant be selected  based on the selected files
+      disabledOptions[key] = optionList.filter((option) => {
+        // Creating temporary filters with the addition current option
+        const tempFilters = { ...filters, [key]: [...filters[key as keyof IDeviceFilters], option] }; 
+        // Checking some device which matches all current filters
+        const isOptionAvailable = devices?.some((device: IFilteredDevicesFromBack) => {
+          const matchesFilters = Object.entries(tempFilters).every(([key, values]) => {
+            if (values.length === 0) return true;
+             //Get the value of device for filter 
+            const deviceValue =
+              key === "manufacturer" ? device.model.manufacturer?.slug :
+              key === "type" ? device.model.type?.slug :
+              key === "model" ? device.model.slug :
+              key === "warehouse" ? device.warehouse?.slug :
+              key === "memorySize" ? String(device.memorySize) :
+              key === "screenSize" ? String(device.screenSize) :
+              key === "isFunctional" ? String(device.isFunctional) :
+              key === "isAssigned" ? String(device.isAssigned) :
+              undefined;
+            return values.includes(deviceValue as keyof IDeviceFilters);
+          });
 
+          return matchesFilters;
+        });
+
+        return !isOptionAvailable;
+      });
+    });
+
+    setDisabledOptions(disabledOptions);
+  }, [filters, devices, options]);
+
+  // Set query params for backend
   useEffect(() => {
     const params: Record<string, string> = {};
     Object.keys(filters).forEach((key: string) => {
@@ -105,62 +104,62 @@ export const useDeviceFilters = () => {
       }));
     }
   };
-
-
+  
+  // Disabled options of the checkboxes
   const getUniqueOptions = (key: keyof IDeviceFilters): CheckedDeviceOptions[] => {
     if (!options) return [];
 
     const optionsMap: Record<keyof IDeviceFilters, any[]> = {
-      manufacturer: options?.manufacturer?.map((option: any, index: number) => ({
+      manufacturer: options.manufacturer?.map((option: any, index: number) => ({
         id: index + 1,
         name: option.name,
         type: option.slug,
         value: option.slug,
         disabled: disabledOptions?.manufacturer?.includes(option.slug),
       })),
-      isFunctional: options?.isFunctional?.map((option: any, index: number) => ({
+      isFunctional: options.isFunctional?.map((option: any, index: number) => ({
         id: index + 1000,
         name: option.isFunctional ? "Да" : "Нет",
         type: "isFunctional",
         value: String(option.isFunctional),
         disabled: disabledOptions?.isFunctional?.includes(String(option.isFunctional)),
       })),
-      type: options?.type?.map((option: any, index: number) => ({
+      type: options.type?.map((option: any, index: number) => ({
         id: index + 2000,
         name: option.name,
         type: option.slug,
         value: option.slug,
         disabled: disabledOptions?.type?.includes(option.slug),
       })),
-      memorySize: options?.memorySize?.map((option: any, index: number) => ({
+      memorySize: options.memorySize?.map((option: any, index: number) => ({
         id: index + 3000,
         name: option.memorySize,
         type: "memorySize",
         value: String(option.memorySize),
         disabled: disabledOptions?.memorySize?.includes(String(option.memorySize)),
       })),
-      screenSize: options?.screenSize?.map((option: any, index: number) => ({
+      screenSize: options.screenSize?.map((option: any, index: number) => ({
         id: index + 4000,
         name: option.screenSize,
         type: "screenSize",
         value: String(option.screenSize),
         disabled: disabledOptions?.screenSize?.includes(String(option.screenSize)),
       })),
-      model: options?.model?.map((option: any, index: number) => ({
+      model: options.model?.map((option: any, index: number) => ({
         id: index + 5000,
         name: option.name,
         type: option.slug,
         value: option.slug,
         disabled: disabledOptions?.model?.includes(option.slug),
       })),
-      warehouse: options?.warehouse?.map((option: any, index: number) => ({
+      warehouse: options.warehouse?.map((option: any, index: number) => ({
         id: index + 6000,
         name: option.name,
         type: option.slug,
         value: option.slug,
         disabled: disabledOptions?.warehouse?.includes(option.slug),
       })),
-      isAssigned: options?.isAssigned?.map((option: any, index: number) => ({
+      isAssigned: options.isAssigned?.map((option: any, index: number) => ({
         id: index + 7000,
         name: option.isAssigned ? "Используется" : "На складе",
         type: "isAssigned",
@@ -174,3 +173,4 @@ export const useDeviceFilters = () => {
 
   return { devices, options, filters, searchParams, handleFilterChange, getUniqueOptions };
 };
+
