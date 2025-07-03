fec0d8e447f44e799115843e138c08736221c066
bartik369 <6apmuk@gmail.com>
2025-07-04 01:08:35 +0300

    fixed media paths

--- src/components/forms/device/DevicePreview.tsx
+++ src/components/forms/device/DevicePreview.tsx
@@ -1,5 +1,6 @@
 import { useAppSelector } from '@/hooks/redux/useRedux';
 import { RootState } from '@/store/store';
+import { PATHS } from '@/utils/constants/system/paths';
 import previewPicture from '@/assets/elements/default.png';
 import styles from "./DeviceForm.module.scss";
 
@@ -8,7 +9,7 @@ const DevicePreview = () => {
     return (
         <figure className={styles.preview}>
         <img src={devicePic
-          ? `/api/models/${devicePic}`
+          ? `${PATHS.models}${devicePic}`
           : previewPicture 
         } alt="" />
       </figure>
