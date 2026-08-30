765b2e8c355d9018310342221410879f7ce4b9b4
bartik369 <6apmuk@gmail.com>
2026-08-30 16:03:44 +0300

    feat(devices): start new filter logic

--- src/widgets/sidebar/ui/SidebarMenu/SidebarMenu.tsx
+++ src/widgets/sidebar/ui/SidebarMenu/SidebarMenu.tsx
@@ -1,26 +1,21 @@
 import { useState } from 'react';
 
-import { IoIosArrowDown, IoIosArrowUp } from 'react-icons/io';
 import { NavLink } from 'react-router-dom';
 
 import { sidebarMenuData } from '../../model/sidebarMenuData';
 import styles from './SidebarMenu.module.scss';
-import SubMenu from './SubMenu';
 
 interface SidebarMenuProps {
   open: boolean;
 }
 
 const SidebarMenu = ({ open }: SidebarMenuProps) => {
-  const [openSubmenu, setOpenSubmenu] = useState<number | null>(null);
   const [hoveredItem, setHoveredItem] = useState<number | null>(null);
 
-  const toggleSubmenu = (id: number) => {
-    setOpenSubmenu(id === openSubmenu ? null : id);
+  const handleMouseEnter = (id: number) => {
+    setHoveredItem(id);
   };
-  const handleMouseEnter = (index: number) => {
-    setHoveredItem(index);
-  };
+
   const handleMouseLeave = () => {
     setHoveredItem(null);
   };
@@ -31,45 +26,21 @@ const SidebarMenu = ({ open }: SidebarMenuProps) => {
         {sidebarMenuData.map((item) => (
           <li key={item.id} className={styles.menuItem}>
             <NavLink
-              to={item.subMenu ? '' : item.path}
+              to={item.path}
               className={({ isActive }) =>
-                `${styles.menuItemHeader} ${
-                  isActive ? styles.activeMenu : item.subMenu ? styles.hasSubmenu : ''
-                }`
+                `${styles.menuItemHeader} ${isActive ? styles.activeMenu : ''}`
               }
-              onClick={() => item.subMenu && toggleSubmenu(item.id)}
               onMouseEnter={() => handleMouseEnter(item.id)}
               onMouseLeave={handleMouseLeave}
             >
               <span className={!open ? styles.icon : styles.openIcon}>
-                {!item.subMenu
-                  ? item.icon && <item.icon className={styles.icon} aria-label={item.title} />
-                  : item.icon && <item.icon className={styles.icon} aria-label={item.title} />}
+                {item.icon && <item.icon className={styles.icon} aria-label={item.title} />}
               </span>
-              {!open && hoveredItem === item.id && !item.subMenu && (
-                <div className={styles.title}>{item.title}</div>
-              )}
-              {open && !item.subMenu ? (
-                <div className={styles.item}>{item.title}</div>
-              ) : open && item.subMenu ? (
-                <div className={styles.subTitle}>{item.title}</div>
-              ) : (
-                ''
-              )}
-              {!open && hoveredItem === item.id && item.subMenu && (
-                <div className={styles.closedSubmenu}>
-                  <SubMenu title={item.title} item={item} open={open} />
-                </div>
-              )}
-              {item.subMenu && open && (
-                <span className={styles.toggleSubmenu}>
-                  {openSubmenu === item.id ? <IoIosArrowUp /> : <IoIosArrowDown />}
-                </span>
-              )}
+
+              {open && <div className={styles.item}>{item.title}</div>}
+
+              {!open && hoveredItem === item.id && <div className={styles.title}>{item.title}</div>}
             </NavLink>
-            {openSubmenu === item.id && item.subMenu && open && (
-              <SubMenu title={item.title} item={item} open={open} />
-            )}
           </li>
         ))}
       </ul>
