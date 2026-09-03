947456206afcaac1d56688b6a04b2e7d211c84d5
bartik369 <6apmuk@gmail.com>
2026-09-03 12:59:11 +0300

    feat(device page): change view part 1

--- src/components/forms/device/DevicePreview.tsx
+++ src/components/forms/device/DevicePreview.tsx
@@ -1,19 +1,17 @@
-import { useAppSelector } from '@/hooks/redux/useRedux';
-import { RootState } from '@/store/store';
-import { PATHS } from '@/utils/constants/system/paths';
 import previewPicture from '@/assets/elements/default.png';
-import styles from "./DeviceForm.module.scss";
+import { useAppSelector } from '@/hooks/redux/useRedux';
+import { PATHS } from '@/shared/api/paths';
+import { RootState } from '@/store/store';
+
+import styles from './DeviceForm.module.scss';
 
 const DevicePreview = () => {
-    const devicePic = useAppSelector((state:RootState) => state.device?.media?.prevImg);
-    return (
-        <figure className={styles.preview}>
-        <img src={devicePic
-          ? `${PATHS.models}${devicePic}`
-          : previewPicture 
-        } alt="" />
-      </figure>
-    );
+  const devicePic = useAppSelector((state: RootState) => state.device?.media?.prevImg);
+  return (
+    <figure className={styles.preview}>
+      <img src={devicePic ? `${PATHS.models}${devicePic}` : previewPicture} alt="" />
+    </figure>
+  );
 };
 
 export default DevicePreview;
